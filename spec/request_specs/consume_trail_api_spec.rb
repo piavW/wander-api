b445b0b81dbca5b807891b5a3f2d1f1da8342f8c
piavw <piavonwachenfelt@hotmail.com>
2019-11-11 11:59:11 +0100

    Creates request-test for api get request, adds Gems: rest-client, pry-rails, pry-byebug.

---
+++ spec/request_specs/consume_trail_api_spec.rb
@@ -0,0 +1,16 @@
+RSpec.describe 'Consume API' do 
+	Before do
+		
+	end
+
+	describe 'API gives data' do 
+		it 'gives a string of trails' do
+			
+			expect(response_json).to eq 'Trail information'
+		end
+
+		it 'gives status 200' do
+			expect(respone.status).to eq 200
+		end
+  end
+end
